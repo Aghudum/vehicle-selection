98d22dca2061008398b3045a18c9b0bb41e867bd
Aghudum <aghudum25@gmail.com>
2015-10-09 17:42:56 +0100

    Update manufacturerGroup.js

--- manufacturerGroup.js
+++ manufacturerGroup.js
@@ -1,30 +1,34 @@
 /**
  * Created by wilok on 05/10/15.
  */
-function ManufacturerGroup(id, name){
-    Node.call(this, id, name);
-    this.manufacturers = ko.observableArray();
-    this.manufacturers.areLoaded = ko.observable();
-    this.isExpanded = ko.observable();
+define('ManufacturerGroup', ['knockout','Manufacturer', 'proxy', 'Node'], function(ko, Manufacturer, proxy, Node){
+    function ManufacturerGroup(id, name){
+        Node.call(this, id, name);
+        this.manufacturers = ko.observable([]);
+        this.manufacturers.areLoaded = ko.observable();
+        this.isExpanded = ko.observable();
 
-    this.toggleExpand = function(){
-        this.isExpanded(!this.isExpanded());
-        if(this.manufacturers.areLoaded()) return;
-        loadManufacturers.call(this);
-        this.manufacturers.areLoaded(true);
-
-    }.bind(this);
+        this.toggleExpand = function(){
+            this.isExpanded(!this.isExpanded());
+            if(!this.manufacturers.areLoaded()) {
+                loadManufacturers.call(this);
+                this.manufacturers.areLoaded(true);
+            };
+        }.bind(this);
+    }
 
     function loadManufacturers(){
+        var manufacturers = [];
         proxy.getManufacturers({}, function(response){
             for (var i = 0; i < response.length; i++) {
                 var model = response[i];
-                addManufacturer.call(this, new Manufacturer(model.Id, model.Name));
+                manufacturers.push(new Manufacturer(model.Id, model.Name));
             }
         }.bind(this));
+        
+        this.manufacturers(manufacturers);
     };
 
-    function addManufacturer(manufacturer){
-        this.manufacturers.push(manufacturer);
-    }
-}
+    return ManufacturerGroup;
+});
+
