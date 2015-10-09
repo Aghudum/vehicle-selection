8e0013d29766be09d95091109f4fececcbb071c2
Aghudum <aghudum25@gmail.com>
2015-10-09 17:43:37 +0100

    Update node.js

--- node.js
+++ node.js
@@ -1,36 +1,11 @@
 /**
  * Created by wilok on 05/10/15.
  */
-function Node(id, name){
-    this.id = ko.observable(id);
-    this.name = ko.observable(name);
-}
-
-function SelectableNode(id, name){
-    Node.call(this, id, name);
-
-    this.statusCss = ko.observable();
-
-    this.isSelected = ko.observable();
-
-    this.select = function(){
-        this.isSelected(true);
+define('Node', ['knockout'], function(ko){
+    function Node(id, name){
+        this.id = ko.observable(id);
+        this.name = ko.observable(name);
     }
 
-    this.deselect = function(){
-        this.isSelected(false);
-    }
-
-    var onToggleCallbacks = [];
-
-    this.toggleSelect = function(){
-        this.isSelected(!this.isSelected());
-        for (var i = 0; i < onToggleCallbacks.length; i++) {
-            onToggleCallbacks[i](this.isSelected());
-        }
-    };
-
-    this.onToggle = function(callback){
-        onToggleCallbacks.push(callback);
-    }
-}
+    return Node;
+});
