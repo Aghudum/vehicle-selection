6bab2621e6b313a1dd64de69c8dc43166057ada0
Aghudum <aghudum25@gmail.com>
2015-10-28 18:53:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,99 @@ require(['VehicleSelector'], function(VehicleSelector){
 	app.init();
 });
 
-define('VehicleSelector', ['ManufacturerGroup', 'knockout'], function(ManufacturerGroup, ko){
+define('VehicleSelector', ['ManufacturerGroup', 'knockout', 'lodash'], function(ManufacturerGroup, ko, _){
 	function VehicleSelector(){
 		this.init = function(){
-		    var vwGroup = new ManufacturerGroup(1, "VW Group");
-		    var nonVwGroup = new ManufacturerGroup(1, "Not VW Group");
+			selection = JSON.parse(localStorage.getItem('selection'));
+
+		    var vwGroup = new ManufacturerGroup(1, "VW Group", selection);
+		    var nonVwGroup = new ManufacturerGroup(1, "Not VW Group", selection);
 
 		    vwGroup.toggleExpand();
 
-		    var audi = vwGroup.manufacturers()[0];
+		    //var audi = vwGroup.manufacturers()[0];
 
-		    var model = audi.models()[0];
+		    //var model = audi.models()[0];
 
-		    ko.applyBindings({groups: [vwGroup, nonVwGroup]}, document.getElementById("vehicle-selector"));
+		    var selector = {
+		    	groups: [vwGroup, nonVwGroup],
+		    	 options: {
+			    	currentOnlySelection: true,
+			    	currentAndFutureSelection: false
+		    	}
+	    	}; 	
+
+		    ko.applyBindings(selector, document.getElementById("vehicle-selector"));
+
+			function add_to_selection(selection, manufacturerId, modelId, variantId, date, isExcluded){
+				selection.push({
+			    	manufacturerId: manufacturerId,
+		            modelId: modelId,
+		            variantId: variantId,
+		            date: date,
+		            isExcluded: isExcluded
+				});
+			}
+
+    		window.save = function(){
+    			var selection = [];
+				_.each(vwGroup.manufacturers(), function(manufacturer){
+					//manufacturer selected
+					if(manufacturer.isSelected()){
+						add_to_selection(selection, manufacturer.id(), null, null, manufacturer.date(), false);
+
+						_.each(manufacturer.models(), function(model){
+							//model selected
+							if(model.isSelected()){
+								_.each(model.variants(), function(variant){
+									//variant NOT selected
+									if(!variant.isSelected()){
+										add_to_selection(selection, manufacturer.id(), model.id(), variant.id(), null, true);
+									}						
+								});
+							}
+							//model NOT selected
+							else{
+								add_to_selection(selection, manufacturer.id(), model.id(), null, model.date(), true);
+
+								_.each(model.variants(), function(variant){
+									//variant selected
+									if(variant.isSelected()){
+										add_to_selection(selection, manufacturer.id(), model.id(), variant.id(), null, false);
+									}						
+								});
+							}
+						});
+					}
+					// manufacturer NOT selected
+					else{
+						_.each(manufacturer.models(), function(model){
+							// model selected
+							if(model.isSelected()){
+								add_to_selection(selection, manufacturer.id(), model.id(), null, model.date(), false);
+
+								_.each(model.variants(), function(variant){
+									//variant NOT selected
+									if(!variant.isSelected()){
+										add_to_selection(selection, manufacturer.id(), model.id(), variant.id(), null, true);
+									}						
+								});
+							}
+							// model NOT selected
+							else{
+								_.each(model.variants(), function(variant){
+									// variant selected
+									if(variant.isSelected()){
+										add_to_selection(selection, manufacturer.id(), model.id(), variant.id(), null, true);
+									}						
+								});
+							}
+						});
+					}
+				});
+
+				localStorage.setItem('selection', JSON.stringify(selection));
+			};
 		};
 	}
 
